9975341d2148efedfce8ab0e1c155f600c3c22da
Little Kafis Software Company <lilweyniwalkermusic@gmail.com>
2026-04-25 16:14:42 +0000

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const pool = require("./db"); // your Neon connection
+const pool = require("./db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 /* TEST ROUTE */
-app.get("/", (req, res) => {
-  res.send("API running...");
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
-
 app.get("/test-db", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
@@ -25,4 +17,8 @@ app.get("/test-db", async (req, res) => {
     console.error(err);
     res.status(500).send("Database error");
   }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
